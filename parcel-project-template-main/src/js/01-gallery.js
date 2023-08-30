43a07151d379d9895b8e1919db326a53590c7ff5
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-08-30 11:19:17 +0200

    w
    w

--- parcel-project-template-main/src/js/01-gallery.js
+++ parcel-project-template-main/src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEL = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  galleryEL.insertAdjacentHTML(
+    'beforeend',
+    `
+      <li class="gallery__item">
+       <a class="gallery__link" href="${item.original}">
+         <img 
+         class="gallery__image" 
+         src="${item.preview}" 
+         alt="${item.description}"          
+       </a>
+      </li>`
+  );
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
