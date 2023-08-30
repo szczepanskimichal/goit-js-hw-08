f1cf03173e18a46997272dd36c6d447f1b8f000a
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-08-30 12:04:06 +0200

    w

--- parcel-project-template-main/src/js/03-feedback.js
+++ parcel-project-template-main/src/js/03-feedback.js
@@ -1,45 +1,42 @@
-import throttle from 'lodash.throttle';
+import throttle from 'lodash/throttle';
 
-const btnEl = document.querySelector('button[type="submit"]');
-const emailEl = document.querySelector('input[type="email"]');
-const messageEl = document.querySelector('textarea[name="message"]');
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
 
-let data = {};
+const STORAGE_KEY = 'feedback-form-state';
 
-if (!localStorage.getItem('feedback-form-state')) {
-  data = {
-    email: '',
-    message: '',
+// Funkcja zapisująca stan formularza do Local Storage
+const saveFormStateToLocalStorage = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
   };
-} else {
-  data = JSON.parse(localStorage.getItem('feedback-form-state'));
-}
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+}, 500);
 
-const checkLocalStorage = () => {
-  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-  if (savedData) {
-    emailEl.value = savedData.email;
-    messageEl.value = savedData.message;
-  } else {
-    emailEl.value = '';
-    messageEl.value = '';
+// Funkcja wczytująca stan formularza z Local Storage i wypełniająca pola
+const loadFormStateFromLocalStorage = () => {
+  const storedState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (storedState) {
+    emailInput.value = storedState.email || '';
+    messageInput.value = storedState.message || '';
   }
 };
 
-checkLocalStorage();
+// Nasłuchiwanie na zmiany w polach formularza i zapisywanie stanu do Local Storage
+emailInput.addEventListener('input', saveFormStateToLocalStorage);
+messageInput.addEventListener('input', saveFormStateToLocalStorage);
 
-function localData(event) {
-  data[event.target.name] = event.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(data));
-}
+// Wczytanie stanu formularza z Local Storage przy ładowaniu strony
+window.addEventListener('load', loadFormStateFromLocalStorage);
 
-function resetLocalSorage(event) {
+// Nasłuchiwanie na przesłanie formularza
+feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
-  localStorage.clear();
-  checkLocalStorage();
-}
-
-emailEl.addEventListener('input', throttle(localData, 1000));
-messageEl.addEventListener('input', throttle(localData, 1000));
-btnEl.addEventListener('click', resetLocalSorage);
+  console.log('Formularz wysłany');
+  localStorage.removeItem(STORAGE_KEY);
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Wyczyszczono stan formularza i Local Storage');
+});
